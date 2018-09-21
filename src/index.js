c4d161865f89b2ca813edadc48c7d598540c9d3e
Justsab89 <justsab89@gmail.com>
2018-09-21 11:38:36 +0600

    мппд

--- src/index.js
+++ src/index.js
@@ -4302,7 +4302,7 @@ connection.query(' SELECT id FROM ?? ORDER BY id DESC LIMIT 1 ',
 
                              var test = [];
                              for(var i = 0; i < rows.length; i++){
-                             test.push([ str_vse[i].begend, str_vse[i].n_zapros, 1989, str_vse[i].id_route, str_vse[i].district, str_vse[i].point_type, str_vse[i].id_street, str_vse[i].street, str_vse[i].id_interception, str_vse[i].interception, str_vse[i].id_point, str_vse[i].busstop, str_vse[i].ordinal, str_vse[i].nearby_interception, str_vse[i].point_parinter_min5, str_vse[i].point_parinter_plu5, str_vse_time[1].time_beg, str_vse_time[1].time_end, 'free', str_vse[i].n_pass, all_districts ]);
+                             test.push([ str_vse[i].begend, str_vse[i].n_zapros, user_id, str_vse[i].id_route, str_vse[i].district, str_vse[i].point_type, str_vse[i].id_street, str_vse[i].street, str_vse[i].id_interception, str_vse[i].interception, str_vse[i].id_point, str_vse[i].busstop, str_vse[i].ordinal, str_vse[i].nearby_interception, str_vse[i].point_parinter_min5, str_vse[i].point_parinter_plu5, str_vse_time[1].time_beg, str_vse_time[1].time_end, 'free', str_vse[i].n_pass, all_districts ]);
 //                             test.push([ str_vse[i].begend, str_vse[i].n_zapros, 1988, str_vse[i].id_route, str_vse[i].district, str_vse[i].point_type, str_vse[i].id_street, str_vse[i].street, str_vse[i].id_interception, str_vse[i].interception, str_vse[i].id_point, str_vse[i].busstop, str_vse[i].ordinal, str_vse[i].nearby_interception, str_vse[i].point_parinter_min5, str_vse[i].point_parinter_plu5, 'free', str_vse[i].n_pass, all_districts ]);
 
                              }
@@ -4344,8 +4344,8 @@ connection.query(' SELECT id FROM ?? ORDER BY id DESC LIMIT 1 ',
 //                 bot.sendPhoto(user_id, fs.readFileSync(__dirname + '/picture-map.png'))
 
 
-                const text = 'По всем направлениям цена 300 тг на одного пассажира\nКроме этих направлений:\nВнутри любого района 200 тг\nРайон Базара - Юго-восток 200 тг\nРайон Базара - Федоровка 200 тг\nМайкудук - Сортировка 200 тг\nУштобе - Юго-восток 200 тг '
-                bot.sendMessage(user_id, text)
+//                const text = 'По всем направлениям цена 300 тг на одного пассажира\nКроме этих направлений:\nВнутри любого района 200 тг\nРайон Базара - Юго-восток 200 тг\nРайон Базара - Федоровка 200 тг\nМайкудук - Сортировка 200 тг\nУштобе - Юго-восток 200 тг '
+//                bot.sendMessage(user_id, text)
 
 //                 pass_offer_topass (query);
 //                 pass_offer_todriv (query);
